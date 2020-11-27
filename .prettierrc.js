bb0c00338e75f73da98375bc271175fe86b6175e
yuuki008 <s22ah261@seinan-gakuin.jp>
2020-11-27 16:49:48 +0900

    fixed: インデント修正

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,16 +2,15 @@ module.exports = {
     jsxBracketSameLine: true,
     singleQuote: true,
     trailingComma: 'all',
-    tabWidth: 4,
     semi: false,
     printWidth: 120,
     parser: 'typescript',
     overrides: [
-      {
-        files: '*.json',
-        options: {
-          parser: 'json',
+        {
+            files: '*.json',
+            options: {
+                parser: 'json',
+            },
         },
-      },
     ],
 }
