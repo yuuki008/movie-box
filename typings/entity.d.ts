457928af97d2023bb0dc8dbb9ee52dd1e587c53f
yuuki008 <s22ah261@seinan-gakuin.jp>
2020-11-28 17:56:45 +0900

    update: 型追加

--- typings/entity.d.ts
+++ typings/entity.d.ts
@@ -1,69 +1,120 @@
 type Genre = {
-    id: number
-    name: string
+  id: number
+  name: string
 }
 
 type Movie = {
-    id: number
-    movieId: string
-    title: string
-    poster_path: string
-    release_date: string
-    genres: Genre[]
-    overview: string
-    timestamp: string
-    vote_average: number
-    name: string
-    title: string
-    backdrop_path: string
-    vote_count: number
-    first_air_date: string
+  id: number
+  movieId: string
+  title: string
+  poster_path: string
+  release_date: string
+  genres: Genre[]
+  overview: string
+  timestamp: string
+  vote_average: number
+  name: string
+  title: string
+  backdrop_path: string
+  vote_count: number
+  first_air_date: string
 }
 
 type Cast = {
-    name: string
-    content: string
-    id: number
-    profile_path: string
-    character: string
+  name: string
+  content: string
+  id: number
+  profile_path: string
+  character: string
 }
 
 type Folder = {
-    created_at: number
-    name: string
-    uid: string
-    id: string
+  created_at: number
+  name: string
+  uid: string
+  id: string
+  list: Movie[]
 }
 
 type Company = {
-    logo_path: string
-    name: string
+  logo_path: string
+  name: string
 }
 
 type Country = {
-    name: string
-    iso_3166_1: string
+  name: string
+  iso_3166_1: string
 }
 
 type Actor = {
-    biography: string
-    birthday: string
-    gender: number
-    name: string
-    profile_path: string
-    place_of_birth: string
-    id: number
-    popularity: number
-    known_for_department: string
+  biography: string
+  birthday: string
+  gender: number
+  name: string
+  profile_path: string
+  place_of_birth: string
+  id: number
+  popularity: number
+  known_for_department: string
 }
 
 type Trailer = {
-    key: string
+  key: string
+}
+
+type DefaultRootState = {
+  movielist: MovieList
+  user: User
+  router: any
+  castlist: CastList
+  trailerlist: TrailerList
+  moviedetail: MovieDetail
+  actordetail: ActorDetail
+  folder: Folder
 }
 
 type User = {
-    isSignedIn: boolean
-    username: string
-    uid: string
-    genres: Genre[]
+  username: string
+  uid: string
+  isSignedIn: boolean
+  genres: Genre[]
+  favorite?: []
+  notifications?: []
+  folder?: []
+}
+
+type MovieList = {
+  isFetching: boolean
+  items: Movie[]
+  page: number
+  total_pages: number
+  error: any
+}
+
+type ActorDetail = {
+  isFetching: boolean
+  item: Actor
+  error: any
+}
+
+type CastList = {
+  isFetching: boolean
+  items: Cast[]
+  error: any
+  page: number
+  total_pages: number
+}
+
+type TrailerList = {
+  isFetching: boolean
+  items: Trailer[]
+  error: any
+  page: number
+  total_pages: number
+}
+
+type MovieDetail = {
+  isFetching: boolean
+  item: Movie
+  error: any
 }
