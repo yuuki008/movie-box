3555e36437a53d7a5f8ee2704d53d74a3e603d70
yuuki008 <s22ah261@seinan-gakuin.jp>
2020-11-28 23:50:13 +0900

    delete: コンポーネント切り分けによるファイル削除

--- src/containers/Auth/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Reset } from './Reset'
-export { default as SignUp } from './SignUp'
-export { default as SignIn } from './SignIn'
--- src/containers/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as Actor} from './Actor'
-export {default as MovieContainer} from './MovieContainer'
-export {default as MovieDetail} from './MovieDetail'
-export {default as MyList} from './MyList'
