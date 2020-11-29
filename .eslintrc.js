49e87e37b6227e0000c275bf270578eb7721bdef
yuuki008 <s22ah261@seinan-gakuin.jp>
2020-11-30 00:10:26 +0900

    fixed: インデント修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/eslint-recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/eslint-recommended'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
-    },
-    plugins: ['react', '@typescript-eslint'],
-    rules: {
-        '@typescript-eslint/indent': 0,
-        '@typescript-eslint/no-var-requires': 0,
-        '@typescript-eslint/explicit-function-return-type': [0],
-        '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
-        '@typescript-eslint/prefer-interface': [0],
-        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-        '@typescript-eslint/camelcase': 0,
-        '@typescript-eslint/ban-ts-ignore': 0,
-    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/explicit-function-return-type': [0],
+    '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
+    '@typescript-eslint/prefer-interface': [0],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/ban-ts-ignore': 0,
+  },
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    jsxBracketSameLine: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    semi: false,
-    printWidth: 120,
-    parser: 'typescript',
-    overrides: [
-        {
-            files: '*.json',
-            options: {
-                parser: 'json',
-            },
-        },
-    ],
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  printWidth: 120,
+  parser: 'typescript',
+  overrides: [
+    {
+      files: '*.json',
+      options: {
+        parser: 'json',
+      },
+    },
+  ],
 }
