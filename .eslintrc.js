72acdd83929f179e7a74c23b83c4189d2dbd8ff8
yuuki008 <s22ah261@seinan-gakuin.jp>
2020-11-30 20:33:08 +0900

    fixed: estends修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requring-type-checking',
   ],
   globals: {
     Atomics: 'readonly',
