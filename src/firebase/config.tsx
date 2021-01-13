fa903d31a10b380e36162b622b7eca81c796265c
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-01-13 17:57:01 +0900

    [fix] firebase繋ぎ直し

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBHs_In-EtCfzH7N7m0T3FPHymNJLQAnIQ',
-  authDomain: 'movie-box-2f18b.firebaseapp.com',
-  databaseURL: 'https://movie-box-2f18b.firebaseio.com',
-  projectId: 'movie-box-2f18b',
-  storageBucket: 'movie-box-2f18b.appspot.com',
-  messagingSenderId: '393494616621',
-  appId: '1:393494616621:web:e725944975b8e93a8536a4',
-  measurementId: 'G-EZV1S7FRJ4',
+  apiKey: 'AIzaSyCM_IrW2JlbZlde3iIt-vZUST1xTP0wtJU',
+  authDomain: 'movi-2e9b3.firebaseapp.com',
+  projectId: 'movi-2e9b3',
+  storageBucket: 'movi-2e9b3.appspot.com',
+  messagingSenderId: '27742065284',
+  appId: '1:27742065284:web:8ac83ca1774e2e4b6a28d1',
+  measurementId: 'G-4P7B3NSQPT',
 }
